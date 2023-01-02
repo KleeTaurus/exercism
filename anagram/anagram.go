4a59c3863ce08e1d21d0f1288bc872ff2ccab73e
Kevin Lee <klee.taurus@gmail.com>
2023-01-02 21:08:49 +0800

    submit anagram solution

--- anagram/anagram.go
+++ anagram/anagram.go
@@ -1,5 +1,46 @@
 package anagram
 
+import (
+	"strings"
+	"unicode"
+)
+
 func Detect(subject string, candidates []string) []string {
-	panic("Please implement the Detect function")
+	anagrams := []string{}
+	for _, candidate := range candidates {
+		if isAnagram(subject, candidate) {
+			anagrams = append(anagrams, candidate)
+		}
+	}
+
+	return anagrams
+}
+
+func isAnagram(subject, candidate string) bool {
+	if len(subject) != len(candidate) {
+		return false
+	}
+
+	if strings.ToLower(subject) == strings.ToLower(candidate) {
+		return false
+	}
+
+	subMap := strToMap(subject)
+	canMap := strToMap(candidate)
+	for k, v := range subMap {
+		if cv, ok := canMap[unicode.ToLower(k)]; !ok || cv != v {
+			return false
+		}
+	}
+
+	return true
+}
+
+func strToMap(str string) map[rune]int {
+	m := make(map[rune]int)
+	for _, s := range str {
+		m[unicode.ToLower(s)]++
+	}
+
+	return m
 }
