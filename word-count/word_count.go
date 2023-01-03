a97b18f011248ab6fc703ee7962c63684f370e77
Kevin Lee <klee.taurus@gmail.com>
2023-01-03 16:53:56 +0800

    optimize the algorithm and make it 3 times faster than regexp implementation

--- word-count/word_count.go
+++ word-count/word_count.go
@@ -10,7 +10,8 @@ type Frequency map[string]int
 var reg = regexp.MustCompile("(\\d+)|(\\w+'?\\w+)|(\\w+)")
 
 func WordCount(phrase string) Frequency {
-	return withRegexp(phrase)
+	// return withRegexp(phrase)
+	return withoutRegexp(phrase)
 }
 
 func withRegexp(phrase string) Frequency {
@@ -20,5 +21,38 @@ func withRegexp(phrase string) Frequency {
 		freq[strings.ToLower(word)]++
 	}
 
+	return freq
+}
+
+func withoutRegexp(phrase string) Frequency {
+	words := []string{}
+	buf := []byte{}
+
+	for _, c := range []byte(phrase) {
+		switch {
+		case c >= '0' && c <= '9':
+			buf = append(buf, c)
+		case (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'):
+			buf = append(buf, c)
+		case c == '\'':
+			buf = append(buf, c)
+		default:
+			if len(buf) > 0 {
+				words = append(words, string(buf))
+				buf = []byte{}
+			}
+		}
+	}
+	if len(buf) > 0 {
+		words = append(words, string(buf))
+	}
+
+	freq := make(Frequency)
+	for _, word := range words {
+		word = strings.Trim(word, "'")
+		if word != "" {
+			freq[strings.ToLower(word)]++
+		}
+	}
 	return freq
 }
